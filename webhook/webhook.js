def0cc9e4b444907080a43aa1f7bf6f30917d099
Jonathan T <thickens@wisc.edu>
2019-12-13 20:47:11 -0600

    cart actions&review/confirm complete. final push

--- webhook/webhook.js
+++ webhook/webhook.js
@@ -14,7 +14,95 @@ app.get('/', (req, res) => res.send('online'))
 app.post('/', express.json(), (req, res) => {
   const agent = new WebhookClient({ request: req, response: res })
 
-async function deleteCart() {
+
+async function getPage() {
+  let request = {
+    method: 'GET',
+    headers: {'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'x-access-token': token,},
+    redirect: 'follow',
+    }
+  const serverReturn = await fetch('https://mysqlcs639.cs.wisc.edu/application/',request);
+  const serverResponse = await serverReturn.json();
+  return serverResponse.page;
+}
+
+async function handleIncrement(id, increments) {
+  let request = {
+    method: 'POST',
+    headers: {'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'x-access-token': token,},
+    redirect: 'follow',
+    }
+  for (let i = 0; i < increments; i++) {
+    await fetch('https://mysqlcs639.cs.wisc.edu/application/products/'+id,request);
+  }
+}
+
+async function handleDecrement(id, increments) {
+  let request = {
+    method: 'DELETE',
+    headers: {'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'x-access-token': token,},
+    redirect: 'follow',
+    }
+  for (let i = 0; i < increments; i++) {
+    await fetch('https://mysqlcs639.cs.wisc.edu/application/products/'+id,request);
+  }
+}
+
+async function modifyCart() {
+  agent.add("modify cart");
+  await postMessage(agent.query, true);
+  let index = agent.parameters.order;
+  let number = agent.parameters.number;
+  let addrm = agent.parameters.addrm;
+  
+  //fetch context
+  await fetchCart();
+  page = await getPage();
+  
+  //check context
+  if (page != ("/"+username+"/cart")) {
+    agent.add("not in cart, can't edit");
+    await postMessage("You have to be in the cart to modify cart contents", false);
+    return;
+  } 
+  //check index provided
+  if (index == "") {
+    await postMessage("I don't know which item to edit", false);
+    return;
+  }
+  //check index valid
+  if(index > cart.length-1) {
+      await postMessage("there aren't that many items in your cart", false);
+      return;
+  }
+  let id = cart[index].id;
+  //check no add w/o number 
+  if (number == "" && addrm == 'add') {
+    await postMessage("I don't know how many to add", false);
+    return;
+  } if (number == "" && addrm == 'remove') {
+      await postMessage("I'll remove this item", false);
+      await handleDecrement(id, cart[index].count);
+      return;  
+  } 
+  if (addrm == 'add') {
+    await postMessage("I'll add "+number+" of the "+cart[index].name, false);
+    await handleIncrement(id, number);
+    return;
+  }if (addrm == 'remove') {
+    await postMessage("I'll remove "+number+" of the "+cart[index].name, false);
+    await handleDecrement(id, number);
+    return;
+  }
+}
+
+  async function deleteCart() {
   agent.add("delete cart");
   await postMessage(agent.query, true);
   let request = {
@@ -125,7 +213,6 @@ async function fetchCart() {
   const serverReturn = await fetch('https://mysqlcs639.cs.wisc.edu/application/products',request);
   let res = await serverReturn.json();
   cart = res.products;
-  console.log(cart);
 }
 
 
@@ -151,7 +238,6 @@ async function navigate() {
     }
   const serverReturn = await fetch('https://mysqlcs639.cs.wisc.edu/application/',request)
   const serverResponse = await serverReturn.json()
-
 }
 
 async function goBack () {
@@ -243,6 +329,22 @@ async function getToken () {
     }
   }
 
+  async function showTags() {
+    await postMessage(agent.query, true);
+    await postMessage("you must be in a category to see the tags", false);
+
+    // let request = {
+    //   method: 'GET',
+    //   headers: {'Accept': 'application/json',
+    //             'Content-Type': 'application/json',
+    //             'x-access-token': token,},
+    //   redirect: 'follow',
+    //   }
+    // const serverReturn = await fetch('https://mysqlcs639.cs.wisc.edu/tags',request);
+    // const serverResponse = await serverReturn.json();
+    // console.log(serverResponse);
+
+  }
 
   let intentMap = new Map();
   intentMap.set('Default Welcome Intent', welcome);
@@ -254,6 +356,8 @@ async function getToken () {
   intentMap.set('CartContents', cartContents);
   intentMap.set('CategoryQuery', categoryQuery);
   intentMap.set('DeleteCart', deleteCart);
+  intentMap.set('ModifyCart', modifyCart);
+  intentMap.set('ShowTags', showTags);
   agent.handleRequest(intentMap);
 })
 
